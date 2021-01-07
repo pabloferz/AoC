076ebc0d7ac33a9632342389d38ce5b35e031fb8
pabloferz <8410335+pabloferz@users.noreply.github.com>
2021-01-07 01:17:14 -0800

    Cleanup day 02

--- 2020/02/test.jl
+++ 2020/02/test.jl
@@ -6,21 +6,19 @@ include("Day02.jl")
 using .Day02
 using Test
 
-import .Day02: split_and_parse
 
-
-const example = [
+const example = Day02.split_and_parse([
     "1-3 a: abcde"
     "1-3 b: cdefg"
     "2-9 c: ccccccccc"
-]
+])
 
 @testset "Part 1" begin
-    @test part1(split_and_parse.(example)) == 2
+    @test part1(example) == 2
 end
 
 @testset "Part 2" begin
-    @test part2(split_and_parse.(example)) == 1
+    @test part2(example) == 1
 end
 
 
